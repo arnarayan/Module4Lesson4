282532d130b448ea4b67c305e8acb335e07abe6e
Anand.Narayan <arnarayan@gmail.com>
2022-02-16 19:11:18 -0700

    Setting up the use of Geometry reader and positioning of rectangles

--- Module4Lesson4/ContentView.swift
+++ Module4Lesson4/ContentView.swift
@@ -9,8 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 0.0) {
+            GeometryReader { geo in
+                Rectangle()
+                    .foregroundColor(.green)
+                    .frame(width: geo.size.width, height: geo.size.height, alignment: .center)
+                    .onTapGesture {
+                        print("x:\(geo.size.width) y:\(geo.size.height)")
+                    }
+                Rectangle()
+                    .foregroundColor(.red)
+                    .frame(width: geo.size.width*2/3, height: geo.size.height*1/4, alignment: .center)
+                    .offset(x: (geo.size.width*1/6), y: geo.size.height*1/2.5)
+                    .onTapGesture {
+                        print("x:\(geo.size.width) y:\(geo.size.height)")
+                    }
+            }.ignoresSafeArea()
+            
+            GeometryReader { geo in
+                Rectangle()
+                .foregroundColor(.purple)
+                    .frame(width: geo.size.width/2, height: geo.size.height, alignment: .center)
+                    .onTapGesture {
+                        print("x:\(geo.size.width) y:\(geo.size.height)")
+                    }
+                    
+                Rectangle()
+                .foregroundColor(.orange)
+                    .frame(width: geo.size.width/2, height: geo.size.height, alignment: .center)
+                    .onTapGesture {
+                        print("x:\(geo.size.width) y:\(geo.size.height)")
+                    }
+                    .offset(x: geo.size.width/2, y:0)
+                    
+                        
+            }.ignoresSafeArea()
+        }
+
+        
+        
+
+
+        
+        
     }
 }
 
